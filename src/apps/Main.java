0b0867d9120ec7e08f9b536a30f62918b04aac4c
Puji Rahmadiyanto <puji.rahmadiyanto@gmail.com>
2011-11-14 16:04:26 +0700

    master

---
+++ src/apps/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package apps;
+
+/**
+ *
+ * @author pujexx
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrameUtama a =new FrameUtama();
+        a.setVisible(true);
+    }
+
+}
